59dc43284d711c73afe97d99072d33c5b8d8db83
Girish Gupta <girishgargdce@gmail.com>
2017-02-20 08:47:22 +0530

    Update logicalOperators.js

--- mongoDB/logical_operators/logical_operators/logicalOperators.js
+++ mongoDB/logical_operators/logical_operators/logicalOperators.js
@@ -1,8 +1,5 @@
-db.movieDetails.find({ $or : [ { "tomato.meter": { $gt: 99 } },
-                               { "metacritic": { $gt: 95 } } ] })
+db.movieDetails.find({ $or : [ { "tomato.meter": { $gt: 99 } }, { "metacritic": { $gt: 95 } } ] })
 
-
-db.movieDetails.find({ $and : [ { "metacritic": { $ne: 100 } },
-                                { "metacritic" { $exists: true } } ] })
+db.movieDetails.find({ $and : [ { "metacritic": { $ne: 100 } }, { "metacritic" : { $exists: true } } ] })
 
 
