0e8f451ae0b5bd1407e50ff03df906cd27d28b6b
gi.gupta <girishgargdce@gmail.com>
2017-02-20 23:39:15 +0530

    code restructuring

--- mongoDB/readcountFromArrayFields.js
+++ mongoDB/readcountFromArrayFields.js
@@ -1,4 +1,4 @@
-Finding the number of fields in an array sub document
+//Finding the number of fields in an array sub document
 
 db.movieDetails.find( { title : /Vampire Film/i } ).toArray()[0].actors.length
 db.movieDetails.findOne( { title : "The Martian" } ).reviews.length
